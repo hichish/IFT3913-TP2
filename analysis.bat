76ea4ac8f3938bf2a76a64f9d14a1035d1728dd3
Hichem Agrane <hichemagrane@gmail.com>
2023-10-27 15:52:58 -0400

    update metric 2

--- analysis.bat
+++ analysis.bat
@@ -16,3 +16,18 @@ echo "On s'attend a au moins 80%"
 echo.
 java -jar branchCoverage.jar ./jfreechart-master
 echo.
+
+
+echo "Question2 : Est-ce que les tests sont à jour avec le reste du code?"
+
+echo "Calcul de la metrique Correspondances des test:"
+echo "On s'attend a au moins 60%"
+echo.
+java -jar TestCorresp.jar ./jfreechart-master
+echo.
+
+echo "Calcul de la metrique Couverture des branches du code source:"
+echo "On s'attend a au moins 70%"
+echo.
+java -jar instrCoverage.jar ./jfreechart-master
+echo.
