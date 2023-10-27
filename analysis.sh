4c00412fd6f07fb1da705d822151636d3cbd1a81
AhmedF03 <[ahmedabdelbary44@gmail.com]>
2023-10-27 16:13:46 -0400

    tDependency,tComments,tMethods done

--- analysis.sh
+++ analysis.sh
@@ -1,33 +1,33 @@
-@echo off
+
 echo "Debut de l'analyse du Projet"
-echo.
+
 
 echo "Question 1: Est-ce qu il y a assez de tests?"
-echo.
+
 
 echo "Calcul de la metrique Correspondances des test:"
 echo "On s'attend a au moins 50%"
-echo.
+
 java -jar TestCorresp.jar ./jfreechart-master
-echo.
+
 
 echo "Calcul de la metrique Couverture des branches du code source:"
 echo "On s'attend a au moins 80%"
-echo.
+
 java -jar branchCoverage.jar ./jfreechart-master
-echo.
+
 
 
 echo "Question2 : Est-ce que les tests sont à jour avec le reste du code?"
 
 echo "Calcul de la metrique Correspondances des test:"
 echo "On s'attend a au moins 60%"
-echo.
+
 java -jar TestCorresp.jar ./jfreechart-master
-echo.
+
 
 echo "Calcul de la metrique Couverture des branches du code source:"
 echo "On s'attend a au moins 70%"
-echo.
+
 java -jar instrCoverage.jar ./jfreechart-master
-echo.
+
